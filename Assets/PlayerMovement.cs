35a02f65d358ec2cec6a1f3462178c8d0fcbb354
Maximilian Rissel <maximilian.rissel@elev.ga.lbs.se>
2018-10-03 09:54:31 +0200

    hej
    Bästa

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * speed*Time.deltaTime, Space.World);
+        }
+    }
+}
